6a254487b714bfecb292f33103542a9efba90811
zclcx521 <zclcx521@163.com>
2025-04-11 09:58:45 +0800

    开发福利页面VIP专属模块：新增VIP独享剧目推荐区域和今日星选功能

--- miniprogram/pages/fuli/fuli.ts
+++ miniprogram/pages/fuli/fuli.ts
@@ -3,27 +3,37 @@ Page({
   data: {
     // 导航栏数据
     navItems: [
-      { id: 1, name: 'VIP专属', active: false },
+      { id: 1, name: 'VIP专属', active: true },
       { id: 2, name: '免费领', active: false },
-      { id: 3, name: '福利社', active: true }
+      { id: 3, name: '福利社', active: false }
     ],
-    // 活动卡片数据
-    activityCards: [
+    // VIP独享剧目数据
+    vipDramas: [
       {
         id: 1,
-        title: '砍剧大联盟',
-        subtitle: '邀请好友一起砍剧',
-        tag: '砍剧',
-        type: 'blue'
+        title: '契约圣女',
+        desc: '飞羽阁最一场突如其来的疾病所苦，恰逢师傅闭关天。在飞羽生死存亡之际，龙虎山男子张伯尘...',
+        cover: '/images/drama1.png'
       },
       {
         id: 2,
-        title: 'N元任购',
-        subtitle: '',
-        tag: '9.9任购3部',
-        type: 'yellow'
+        title: '绝世小狂妃',
+        desc: '女主蓉今月从小被给牙婆子，受尽了屈辱。好不容易逃出来后，却又被人杀害。然而幸运的是，她意外重生到了南莫国公府...',
+        cover: '/images/drama2.png'
+      },
+      {
+        id: 3,
+        title: '女总裁的同居男友',
+        desc: '女主戴安娜为了避免以后再被安排相亲，参加了商务酒局，结果喝得酩酊大醉，不省人事的她先意...',
+        cover: '/images/drama3.png'
       }
-    ]
+    ],
+    // 今日星选数据
+    todayPick: {
+      id: 2,
+      title: '绝世小狂妃',
+      desc: '女主蓉今月从小被给牙婆子，受尽了屈辱。好不容易逃出来后，却又被人杀害。然而幸运的是，她意外重生到了南莫国公府...'
+    }
   },
   
   onLoad() {
